b5c2375be29b54011feada46f2e861f44094b9fa
Jonathan91199 <jonathan.c911@gmail.com>
2021-04-28 22:53:49 +0300

    styling SideBar

--- capitol.gui/src/Components/SideBar/SideBar.js
+++ capitol.gui/src/Components/SideBar/SideBar.js
@@ -1,6 +1,7 @@
 import './Style/SideBarStyle.css'
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { } from '@material-ui/icons/'
 
 class SideBar extends Component {
 
@@ -8,6 +9,16 @@ class SideBar extends Component {
     render() {
         return (
             <div id="SideBarMainDiv" className="SideBarMainDiv">
+                <div className="SideBarHeader">Options</div>
+                <div className="SideBarOptions">
+                    <div className="SideBarOption">+ Add System</div>
+                    <div className="SideBarOption">Edit System</div>
+                </div>
+                <div className="SideBarSettingsDiv">
+                    <div className="SideBarSettingsInnerDiv">
+                        <i className="bi bi-gear-wide-connected"></i>
+                    </div>
+                </div>
 
             </div>
         )
